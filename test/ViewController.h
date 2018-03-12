1692c76eb6a2da28484c85f1006a602786ba52a0
Peng <c18609236670@163.com>
2018-03-12 11:22:23 +0800

    添加git工程

---
+++ test/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Jerry on 2018/2/24.
+//  Copyright © 2018年 peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by Jerry on 2018/2/24.
+//  Copyright © 2018年 peng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+// 写入plist文件
+
+-(void)creatPlist{
+    
+}
+@end
